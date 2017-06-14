86b5a6b0c4857d9de8d04c8913723f397f70d265
awmleer <awmleer@sparker.xyz>
2017-06-14 21:37:45 +0800

    web module done

--- web.py
+++ web.py
@@ -1,12 +1,22 @@
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
-
+import core
 app = Flask(__name__)
+
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@app.route('/execute/',methods=['POST'])
+def execute():
+    sql=request.form['sql']
+    result=core.execute(sql)
+    if type(result['payload'])==str:
+        result['type']='str'
+    if type(result['payload'])==dict:
+        result['type']='dict'
+    return render_template('result.html',result=result,sql=sql)
 
 
 
